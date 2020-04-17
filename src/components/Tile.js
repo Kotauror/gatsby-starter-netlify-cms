5a291194287506e2d387ca93c76ad0fbe3f201b1
Kotauror <zygmunt.justyna@gmail.com>
2020-04-17 16:18:54 +0100

    Show additional info.

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
@@ -7,27 +7,35 @@ const getDate = date => {
   return myDate.toLocaleString();
 };
 
-const Tile = ({ item }) => (
-  <div key={item.hospitalDescription} className="column is-4">
-    <section className="section hospital-tile">
-      <div className="">
-        <div
-          style={{
-            width: "180px",
-            display: "inline-block"
-          }}
-        >
-          <a href={item.link} target="blank" className="clinic-image">
-            <PreviewCompatibleImage imageInfo={item} />
-          </a>
+const Tile = ({ item }) => {
+  const [showMoreInfo, setShowMoreInfo] = useState(false);
+
+  return (
+    <div key={item.hospitalDescription} className="column is-4">
+      <section className="section hospital-tile">
+        <div className="">
+          <div
+            style={{
+              width: "180px",
+              display: "inline-block"
+            }}
+          >
+            <a href={item.link} target="blank" className="clinic-image">
+              <PreviewCompatibleImage imageInfo={item} />
+            </a>
+          </div>
         </div>
-      </div>
-      <h5 className="clinic-name">{item.clinicName}</h5>
-      <p>{item.hospitalDescription}</p>
-      <p>{item.rating}</p>
-      <p>{getDate(item.date)}</p>
-    </section>
-  </div>
-);
+        <h5 className="clinic-name">{item.clinicName}</h5>
+        <p>{item.hospitalDescription}</p>
+        <p>{item.rating}</p>
+        <p>{getDate(item.date)}</p>
+        <button onClick={() => setShowMoreInfo(!showMoreInfo)}>
+          Learn more
+        </button>
+        <p>{showMoreInfo && <p>{item.additionalInformation}</p>}</p>
+      </section>
+    </div>
+  );
+};
 
 export default Tile;
